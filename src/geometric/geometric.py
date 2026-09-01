1b6910d6812678c58ca7f7898104d11302b2285d
JuanRosas45 <315158056+JuanRosas45@users.noreply.github.com.>
2026-08-31 22:05:15 -0500

    millos correcion

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -94,9 +94,13 @@ class Geometria:
         return (y2 - y1) / (x2 - x1)
 
     def ecuacion_recta(self, x1, y1, x2, y2):
-        A = y1 - y2
-        B = x2 - x1
-        C = x1 * y2 - x2 * y1
+        A = y2 - y1
+        B = x1 - x2
+        C = x2 * y1 - x1 * y2
+
+        if A == 0:
+            B = 1
+            C = -y1
 
         return (A, B, C)
 
